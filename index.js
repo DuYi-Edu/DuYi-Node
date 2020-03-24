dbca958873f73a8d3872a5ae8ca19fd4ec36c9a5
yuanjin <yuanjinmoke@gmail.com>
2020-03-24 10:31:03 +0800

    finish

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log(__dirname);
+
+// console.log("当前命令行：", process.cwd());
+
+// setTimeout(() => {
+//   console.log("abc");
+// }, 1000);
+
+// process.exit();
+
+// console.log(process.argv);
+
+// console.log(process.platform);
+
+// process.kill(10428);
+
+console.log(process.env.ANDROID_HOME);
