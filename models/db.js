75be514ffd464609663073f752bdb6a9b65cd596
yjisme <277739025@qq.com>
2020-04-16 11:58:34 +0800

    ok

--- models/db.js
+++ models/db.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
-
+const { sqlLogger } = require("../logger");
 const sequelize = new Sequelize("myschooldb", "root", "ybybdwyJ42.", {
   host: "localhost",
   dialect: "mysql",
-  logging: null
+  logging: (msg) => {
+    sqlLogger.debug(msg);
+  },
 });
 
 module.exports = sequelize;
