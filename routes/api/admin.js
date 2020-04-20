eecb0f857376c30f71d0bf824a568aba733d967c
yjisme <277739025@qq.com>
2020-04-20 17:12:41 +0800

    ok

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,14 +2,22 @@ const express = require("express");
 const router = express.Router();
 const adminServ = require("../../services/adminService");
 const { asyncHandler } = require("../getSendResult");
+const cryptor = require("../../util/crypt");
 
 router.post(
   "/login",
   asyncHandler(async (req, res) => {
     const result = await adminServ.login(req.body.loginId, req.body.loginPwd);
-    if(result){
-        //登录成功
-        res.header("set-cookie", `token=${result.id}; path=/; domain=localhost; max-age=3600; httponly`);
+    if (result) {
+      let value = result.id;
+      value = cryptor.encrypt(value.toString());
+      //登录成功
+      res.cookie("token", value, {
+        path: "/",
+        domain: "localhost",
+        maxAge: 7 * 24 * 3600 * 1000, //毫秒数
+      });
+      res.header("authorization", value);
     }
     return result;
   })
--- routes/init.js
+++ routes/init.js
@@ -6,6 +6,15 @@ const path = require("path");
 const staticRoot = path.resolve(__dirname, "../public");
 app.use(express.static(staticRoot));
 
+// 加入cookie-parser 中间件
+// 加入之后，会在req对象中注入cookies属性，用于获取所有请求传递过来的cookie
+// 加入之后，会在res对象中注入cookie方法，用于设置cookie
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// 应用token中间件
+app.use(require("./tokenMiddleware"));
+
 // 解析 application/x-www-form-urlencoded 格式的请求体
 app.use(express.urlencoded({ extended: true }));
 
