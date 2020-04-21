ca6d496e2268ea51f286063807e0d8cf158a37ea
yjisme <277739025@qq.com>
2020-04-21 13:49:17 +0800

    ok

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,20 @@
-console.log("abc");
+// fetch("http://localhost:5008/api/student")
+//   .then((resp) => resp.json())
+//   .then((resp) => {
+//     console.log(resp);
+//   });
+
+function jsonp(url) {
+  const script = document.createElement("script");
+  script.src = url;
+  document.body.appendChild(script);
+  script.onload = function () {
+    script.remove();
+  };
+}
+
+function callback(data) {
+  console.log(data);
+}
+
+jsonp("http://localhost:5008/api/student");
