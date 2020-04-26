a4b27e9b91d22fd6ed6183d001a2232282cdc3c0
yjisme <277739025@qq.com>
2020-04-26 16:10:38 +0800

    ok

--- routes/errorMiddleware.js
+++ routes/errorMiddleware.js
@@ -4,7 +4,6 @@ const multer = require("multer");
 module.exports = (err, req, res, next) => {
   if (err) {
     if (err instanceof multer.MulterError) {
-      console.log(err);
       res.status(200).send(getMsg.getErr(err.message));
       return;
     }
