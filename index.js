f14ada4ccd75b7cc338b46bbbe85eba6ffaf15bb
yjisme <277739025@qq.com>
2020-05-07 14:06:36 +0800

    ok

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const net = require("net");
+const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
+const path = require("path");
 
-const server = net.createServer((socket) => {
-  console.log("收到客户端的连接");
-  socket.once("data", (chunk) => {
-    const httpContent = chunk.toString("utf-8");
-    let parts = httpContent.split("\r\n");
-    parts.shift();
-    parts = parts
-      .filter((s) => s)
-      .map((s) => {
-        const i = s.indexOf(":");
-        return [s.substr(0, i), s.substr(i + 1).trim()];
-      });
-    const headers = Object.fromEntries(parts);
-    const crypto = require("crypto");
-    const hash = crypto.createHash("sha1");
-    hash.update(
-      headers["Sec-WebSocket-Key"] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-    );
-    const key = hash.digest("base64");
-    // 响应
-    socket.write(`HTTP/1.1 101 Switching Protocols
-Upgrade: websocket
-Connection: Upgrade
-Sec-WebSocket-Accept: ${key}
+// express
+const app = express();
+const server = http.createServer(app);
+app.use(express.static(path.resolve(__dirname, "public")));
 
-`);
-    socket.on("data", (chunk) => {
-      console.log(chunk);
-    });
+// websocket
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  // 当有一个新的客户端连接到服务器成功之后，触发的事件
+  console.log("新的客户端连接进来了");
+  socket.on("msg", (chunk) => {
+    // 监听客户端的msg消息
+    console.log(chunk.toString("utf-8"));
+  });
+  const timer = setInterval(function () {
+    //每隔两秒钟，发送一个消息给客户端，消息为test
+    socket.emit("test", "test message from server");
+  }, 2000);
+
+  socket.on("disconnect", () => {
+    clearInterval(timer);
+    console.log("closed");
   });
 });
 
-server.listen(5008);
+// 监听端口
+server.listen(5008, () => {
+  console.log("server listening on 5008");
+});
